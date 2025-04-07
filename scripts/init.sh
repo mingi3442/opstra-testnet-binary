470eea475b548995fac28a647b044b09f396637d
Lee MinGi <mingi3442@gmail.com>
2025-04-07 15:10:43 +0900

    🔨 Update script

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,17 +7,17 @@ opstrad init $(hostname) --chain-id opstra-testnet-1
 
 # 계정 생성
 echo "==> Create account..."
-opstrad keys add validator --keyring-backend ${whoami}
-opstrad keys add user --keyring-backend ${whoami}
+opstrad keys add validator --keyring-backend $(whoami)
+opstrad keys add user --keyring-backend $(whoami)
 
 # 토큰 할당
 echo "==> Assign tokens..."
-opstrad genesis add-genesis-account validator 10000000uopstra --keyring-backend ${whoami}
-opstrad genesis add-genesis-account user 1000uopstra --keyring-backend ${whoami}
+opstrad genesis add-genesis-account validator 10000000uopstra --keyring-backend $(whoami)
+opstrad genesis add-genesis-account user 1000uopstra --keyring-backend $(whoami)
 
 # validator 생성
 echo "==> Create validator..."
-opstrad genesis gentx validator 1000000uopstra --chain-id opstra-testnet-1 --keyring-backend ${whoami}
+opstrad genesis gentx validator 1000000uopstra --chain-id opstra-testnet-1 --keyring-backend $(whoami)
 
 # gentx 수집
 echo "==> Collect gentxs..."
