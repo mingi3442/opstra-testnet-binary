0c8fe4612ae310995c4bea3ec6e22cb6730bb098
Lee MinGi <mingi3442@gmail.com>
2025-04-07 15:11:19 +0900

    💡 Update comments

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-# init.sh - 노드 초기화 스크립트
+
 
 
 echo "==> Initialize node..."
 opstrad init $(hostname) --chain-id opstra-testnet-1
 
-# 계정 생성
+# Create account
 echo "==> Create account..."
 opstrad keys add validator --keyring-backend $(whoami)
 opstrad keys add user --keyring-backend $(whoami)
 
-# 토큰 할당
+# Assign tokens
 echo "==> Assign tokens..."
 opstrad genesis add-genesis-account validator 10000000uopstra --keyring-backend $(whoami)
 opstrad genesis add-genesis-account user 1000uopstra --keyring-backend $(whoami)
 
-# validator 생성
+# Create validator
 echo "==> Create validator..."
 opstrad genesis gentx validator 1000000uopstra --chain-id opstra-testnet-1 --keyring-backend $(whoami)
 
-# gentx 수집
+# Collect gentxs
 echo "==> Collect gentxs..."
 opstrad genesis collect-gentxs
 
